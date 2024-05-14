fca0ec012c57db96acee672d4cb66af3f51e7958
Bernie Petters <bp4924@gmail.com>
2024-05-14 06:21:08 -0400

    static assets

--- myApp.js
+++ myApp.js
@@ -7,4 +7,6 @@ console.log("Hello World");
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
+app.use("/public", express.static(__dirname + "/public"));
+
 module.exports = app;
